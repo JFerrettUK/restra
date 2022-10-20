830b6e5a7effe43aa3fb095971af7251b2fcb1a7
JFerrettUK <jferrettuk@gmail.com>
2022-10-20 12:37:20 +0100

    Appending elements to the DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("woop!")
+//make a div
+let divElement = document.createElement('div');
+
+//main parent element
+let contentElement = document.getElementById('content');
+contentElement.id = "content";
+
+
+//create the main parent directories
+let contactHead = divElement;
+contactHead.id = "contactHead";
+console.log(contactHead)
+
+// let navMenu = divElement;
+// let contentMain = divElement;
+// let bottomBanner = divElement;
+
+contentElement.appendChild(contactHead);
+
+//create the ContactHead tree
+
+
+
+//create the navMenu tree
+
+
+
+//create the contentMain tree
+
+
+
+//create the bottomBanner tree
